1ea1acb3817c8e567135b57836c6fe52210b179e
Nathanael Madilu <85607797+nathanmadilu@users.noreply.github.com>
2024-03-01 12:33:05 +0300

    Update and rename madilu.txt to index.php

---
+++ My Demo Project/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Under Construction</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Our Website is Under Construction</h1>
+        <p>We'll be here soon with our new awesome site.</p>
+    </div>
+</body>
+</html>
